fa34d3f95f2b1d1e91ef4e5825020e939d0e2091
Shayan Shiravani <shayan6959@gmail.com>
2025-01-11 17:16:20 +0330

    Validate operator sig

--- contracts/libs/EigenLayerVerifier.sol
+++ contracts/libs/EigenLayerVerifier.sol
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {SignatureChecker} from "openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 library EigenLayerVerifier {
-    using SignatureChecker for address;
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
 
-    /// @notice Validates a given signature against the signer's address and data hash.
-    /// @param _signer The address of the signer to validate.
-    /// @param _dataHash The hash of the data that is signed.
-    /// @param _signature The signature to validate.
     function _validateSignature(
-        address _signer,
-        bytes32 _dataHash,
-        bytes memory _signature
-    ) internal view {
-        if (!_signer.isValidSignatureNow(_dataHash, _signature)) {
-            revert("Invalid EigenLayer signature!");
-        }
+        address operator,
+        bytes32 hash,
+        bytes calldata signature
+    ) internal pure {
+        hash = hash.toEthSignedMessageHash();
+        address signatureSigner = hash.recover(signature);
+
+        require(signatureSigner == operator, "Invalid EigenLayer signature!");
     }
 }
