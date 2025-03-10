8b751fca967851430a8db3293e4183050ccf82ee
Shayan Shiravani <shayan6959@gmail.com>
2025-03-10 18:09:41 +0330

    stake registry

---
+++ contracts/MuonStakeRegistry.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import {ISignatureUtils} from "eigenlayer-contracts/src/contracts/interfaces/ISignatureUtils.sol";
+import {IDelegationManager} from "eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
+import {ECDSAStakeRegistry} from "eigenlayer/src/unaudited/ECDSAStakeRegistry.sol";
+import {IMuonServiceManager} from "./interfaces/IMuonServiceManager.sol";
+
+/// @title MUON Stake Registry
+contract MuonStakerRegistry is ECDSAStakeRegistry {
+    constructor(
+        IDelegationManager _delegationManager
+    ) ECDSAStakeRegistry(_delegationManager) {
+        _disableInitializers();
+    }
+
+    /// @notice request to exit AVS
+    function requestExit() external {
+        IMuonServiceManager(_serviceManager).requestExit(msg.sender);
+    }
+}
